3c9a5c2350f1f7487cbc5e205a3af13e8b210063
brenno <brennorebeiro@gmail.com>
2024-11-30 01:22:01 -0300

    adiciona model do questionario

--- apps/questionario/models.py
+++ apps/questionario/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class TbQuestionario(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    idade = models.IntegerField(null=False)
+    questao_1 = models.IntegerField(null=False)
+    questao_2 = models.IntegerField(null=False)
+    questao_3 = models.IntegerField(null=False)
+    questao_4 = models.IntegerField(null=False)
+    questao_5 = models.IntegerField(null=False)
+    questao_6 = models.IntegerField(null=False)
+    questao_7 = models.IntegerField(null=False)
+    questao_8 = models.TextField(null=False, blank=False)
+    questao_9 = models.TextField(null=False, blank=False)
+    questao_10 = models.TextField(null=False, blank=False)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['id']
+
+        db_table = 'tb_questionario'
