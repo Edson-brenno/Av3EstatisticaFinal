bdd3cda194d2d54b400e52f38b9d2789233b2952
brenno <brennorebeiro@gmail.com>
2024-12-04 00:42:59 -0300

    fix moda

--- apps/relatorio/views.py
+++ apps/relatorio/views.py
@@ -16,46 +16,46 @@ def obter_dados_grafico(request):
 
     dados_retorno_grafico = {
         "grafico_questao_1": {
-            "altissima_frequencia": dados.filter(questao_1 = 1).count(),
-            "alta_frequencia": dados.filter(questao_1 = 2).count(),
+            "altissima_frequencia": dados.filter(questao_1 = 5).count(),
+            "alta_frequencia": dados.filter(questao_1 = 4).count(),
             "frequente": dados.filter(questao_1 = 3).count(),
-            "baixa_frequencia": dados.filter(questao_1 = 4).count(),
-            "baixissima_frequencia": dados.filter(questao_1 = 5).count(),
+            "baixa_frequencia": dados.filter(questao_1 = 2).count(),
+            "baixissima_frequencia": dados.filter(questao_1 = 1).count(),
         },
         "grafico_questao_2": {
-            "altissima_frequencia": dados.filter(questao_2=1).count(),
-            "alta_frequencia": dados.filter(questao_2=2).count(),
+            "altissima_frequencia": dados.filter(questao_2=5).count(),
+            "alta_frequencia": dados.filter(questao_2=4).count(),
             "frequente": dados.filter(questao_2=3).count(),
-            "baixa_frequencia": dados.filter(questao_2=4).count(),
-            "baixissima_frequencia": dados.filter(questao_2=5).count(),
+            "baixa_frequencia": dados.filter(questao_2=2).count(),
+            "baixissima_frequencia": dados.filter(questao_2=1).count(),
         },
         "grafico_questao_3": {
-            "altissima_importancia": dados.filter(questao_3=1).count(),
-            "alta_importancia": dados.filter(questao_3=2).count(),
+            "altissima_importancia": dados.filter(questao_3=5).count(),
+            "alta_importancia": dados.filter(questao_3=4).count(),
             "importante": dados.filter(questao_3=3).count(),
-            "baixa_importancia": dados.filter(questao_3=4).count(),
-            "baixissima_importancia": dados.filter(questao_3=5).count(),
+            "baixa_importancia": dados.filter(questao_3=2).count(),
+            "baixissima_importancia": dados.filter(questao_3=1).count(),
         },
         "grafico_questao_4": {
-            "altamente_suficiente": dados.filter(questao_4=1).count(),
-            "muito_suficiente": dados.filter(questao_4=2).count(),
+            "altamente_suficiente": dados.filter(questao_4=5).count(),
+            "muito_suficiente": dados.filter(questao_4=4).count(),
             "suficiente": dados.filter(questao_4=3).count(),
-            "pouco_suficiente": dados.filter(questao_4=4).count(),
-            "nada_suficiente": dados.filter(questao_4=5).count(),
+            "pouco_suficiente": dados.filter(questao_4=2).count(),
+            "nada_suficiente": dados.filter(questao_4=1).count(),
         },
         "grafico_questao_5": {
-            "altamente_suficiente": dados.filter(questao_5=1).count(),
-            "muito_suficiente": dados.filter(questao_5=2).count(),
+            "altamente_suficiente": dados.filter(questao_5=5).count(),
+            "muito_suficiente": dados.filter(questao_5=4).count(),
             "suficiente": dados.filter(questao_5=3).count(),
-            "pouco_suficiente": dados.filter(questao_5=4).count(),
-            "nada_suficiente": dados.filter(questao_5=5).count(),
+            "pouco_suficiente": dados.filter(questao_5=2).count(),
+            "nada_suficiente": dados.filter(questao_5=1).count(),
         },
         "grafico_questao_6": {
-            "reduzir_uso_de_plastico": dados.filter(questao_6=1).count(),
-            "aumentar_campanhas": dados.filter(questao_6=2).count(),
+            "reduzir_uso_de_plastico": dados.filter(questao_6=5).count(),
+            "aumentar_campanhas": dados.filter(questao_6=4).count(),
             "incentivar_particas": dados.filter(questao_6=3).count(),
-            "reforcar_leis": dados.filter(questao_6=4).count(),
-            "melhorar_saneamento": dados.filter(questao_6=5).count(),
+            "reforcar_leis": dados.filter(questao_6=2).count(),
+            "melhorar_saneamento": dados.filter(questao_6=1).count(),
         },
     }
 
@@ -74,28 +74,28 @@ def converter_para_python(valor):
 def obter_dados_dispersao(request):
 
     dados = TbQuestionario.objects.all()
-    dados_dispersao_questao_1 = [dados.filter(questao_1=1).count(), dados.filter(questao_1=2).count(), dados.filter(questao_1=3).count(),
-                                 dados.filter(questao_1=4).count(), dados.filter(questao_1=5).count()]
+    dados_dispersao_questao_1 = [dados.filter(questao_1=5).count(), dados.filter(questao_1=4).count(), dados.filter(questao_1=3).count(),
+                                 dados.filter(questao_1=2).count(), dados.filter(questao_1=1).count()]
 
-    dados_dispersao_questao_2 = [dados.filter(questao_2=1).count(), dados.filter(questao_2=2).count(),
+    dados_dispersao_questao_2 = [dados.filter(questao_2=5).count(), dados.filter(questao_2=4).count(),
                                  dados.filter(questao_2=3).count(),
-                                 dados.filter(questao_2=4).count(), dados.filter(questao_2=5).count()]
+                                 dados.filter(questao_2=2).count(), dados.filter(questao_2=1).count()]
 
-    dados_dispersao_questao_3 = [dados.filter(questao_3=1).count(), dados.filter(questao_3=2).count(),
+    dados_dispersao_questao_3 = [dados.filter(questao_3=5).count(), dados.filter(questao_3=4).count(),
                                  dados.filter(questao_3=3).count(),
-                                 dados.filter(questao_3=4).count(), dados.filter(questao_3=5).count()]
+                                 dados.filter(questao_3=2).count(), dados.filter(questao_3=1).count()]
 
-    dados_dispersao_questao_4 = [dados.filter(questao_4=1).count(), dados.filter(questao_4=2).count(),
+    dados_dispersao_questao_4 = [dados.filter(questao_4=5).count(), dados.filter(questao_4=4).count(),
                                  dados.filter(questao_4=3).count(),
-                                 dados.filter(questao_4=4).count(), dados.filter(questao_4=5).count()]
+                                 dados.filter(questao_4=2).count(), dados.filter(questao_4=1).count()]
 
-    dados_dispersao_questao_5 = [dados.filter(questao_5=1).count(), dados.filter(questao_5=2).count(),
+    dados_dispersao_questao_5 = [dados.filter(questao_5=5).count(), dados.filter(questao_5=4).count(),
                                  dados.filter(questao_5=3).count(),
-                                 dados.filter(questao_5=4).count(), dados.filter(questao_5=5).count()]
+                                 dados.filter(questao_5=2).count(), dados.filter(questao_5=1).count()]
 
-    dados_dispersao_questao_6 = [dados.filter(questao_6=1).count(), dados.filter(questao_6=2).count(),
+    dados_dispersao_questao_6 = [dados.filter(questao_6=5).count(), dados.filter(questao_6=4).count(),
                                  dados.filter(questao_6=3).count(),
-                                 dados.filter(questao_6=4).count(), dados.filter(questao_6=5).count()]
+                                 dados.filter(questao_6=2).count(), dados.filter(questao_6=1).count()]
 
     # Construindo os dados e convertendo os valores
     dados_retorno_dispersao = {
@@ -159,28 +159,28 @@ def obter_dados_tendencia(request):
 
     dados = TbQuestionario.objects.all()
 
-    dados_tendencia_questao_1 = [dados.filter(questao_1=1).count(), dados.filter(questao_1=2).count(), dados.filter(questao_1=3).count(),
-                                 dados.filter(questao_1=4).count(), dados.filter(questao_1=5).count()]
+    dados_tendencia_questao_1 = [dados.filter(questao_1=5).count(), dados.filter(questao_1=4).count(), dados.filter(questao_1=3).count(),
+                                 dados.filter(questao_1=2).count(), dados.filter(questao_1=1).count()]
 
-    dados_tendencia_questao_2 = [dados.filter(questao_2=1).count(), dados.filter(questao_2=2).count(),
+    dados_tendencia_questao_2 = [dados.filter(questao_2=5).count(), dados.filter(questao_2=4).count(),
                                  dados.filter(questao_2=3).count(),
-                                 dados.filter(questao_2=4).count(), dados.filter(questao_2=5).count()]
+                                 dados.filter(questao_2=2).count(), dados.filter(questao_2=1).count()]
 
-    dados_tendencia_questao_3 = [dados.filter(questao_3=1).count(), dados.filter(questao_3=2).count(),
+    dados_tendencia_questao_3 = [dados.filter(questao_3=5).count(), dados.filter(questao_3=4).count(),
                                  dados.filter(questao_3=3).count(),
-                                 dados.filter(questao_3=4).count(), dados.filter(questao_3=5).count()]
+                                 dados.filter(questao_3=2).count(), dados.filter(questao_3=1).count()]
 
-    dados_tendencia_questao_4 = [dados.filter(questao_4=1).count(), dados.filter(questao_4=2).count(),
+    dados_tendencia_questao_4 = [dados.filter(questao_4=5).count(), dados.filter(questao_4=4).count(),
                                  dados.filter(questao_4=3).count(),
-                                 dados.filter(questao_4=4).count(), dados.filter(questao_4=5).count()]
+                                 dados.filter(questao_4=2).count(), dados.filter(questao_4=1).count()]
 
-    dados_tendencia_questao_5 = [dados.filter(questao_5=1).count(), dados.filter(questao_5=2).count(),
+    dados_tendencia_questao_5 = [dados.filter(questao_5=5).count(), dados.filter(questao_5=4).count(),
                                  dados.filter(questao_5=3).count(),
-                                 dados.filter(questao_5=4).count(), dados.filter(questao_5=5).count()]
+                                 dados.filter(questao_5=2).count(), dados.filter(questao_5=1).count()]
 
-    dados_tendencia_questao_6 = [dados.filter(questao_6=1).count(), dados.filter(questao_6=2).count(),
+    dados_tendencia_questao_6 = [dados.filter(questao_6=5).count(), dados.filter(questao_6=4).count(),
                                  dados.filter(questao_6=3).count(),
-                                 dados.filter(questao_6=4).count(), dados.filter(questao_6=5).count()]
+                                 dados.filter(questao_6=2).count(), dados.filter(questao_6=1).count()]
 
     # Construindo os dados e convertendo os valores
     # Construindo os dados e convertendo os valores
@@ -188,32 +188,32 @@ def obter_dados_tendencia(request):
         "tendencia_questao_1": {
             "media": converter_para_python(nb.mean(dados_tendencia_questao_1)),
             "mediana": converter_para_python(nb.median(dados_tendencia_questao_1)),
-            "moda": obter_moda(dados_tendencia_questao_1),
+            "moda": dados_tendencia_questao_1.index(max(dados_tendencia_questao_1)) + 1,
         },
         "tendencia_questao_2": {
             "media": converter_para_python(nb.mean(dados_tendencia_questao_2)),
             "mediana": converter_para_python(nb.median(dados_tendencia_questao_2)),
-            "moda": obter_moda(dados_tendencia_questao_2),
+            "moda": dados_tendencia_questao_2.index(max(dados_tendencia_questao_2)) + 1,
         },
         "tendencia_questao_3": {
             "media": converter_para_python(nb.mean(dados_tendencia_questao_3)),
             "mediana": converter_para_python(nb.median(dados_tendencia_questao_3)),
-            "moda": obter_moda(dados_tendencia_questao_3),
+            "moda": dados_tendencia_questao_3.index(max(dados_tendencia_questao_3)) + 1,
         },
         "tendencia_questao_4": {
             "media": converter_para_python(nb.mean(dados_tendencia_questao_4)),
             "mediana": converter_para_python(nb.median(dados_tendencia_questao_4)),
-            "moda": obter_moda(dados_tendencia_questao_4),
+            "moda": dados_tendencia_questao_4.index(max(dados_tendencia_questao_4)) + 1,
         },
         "tendencia_questao_5": {
             "media": converter_para_python(nb.mean(dados_tendencia_questao_5)),
             "mediana": converter_para_python(nb.median(dados_tendencia_questao_5)),
-            "moda": obter_moda(dados_tendencia_questao_5),
+            "moda": dados_tendencia_questao_5.index(max(dados_tendencia_questao_5)) + 1,
         },
         "tendencia_questao_6": {
             "media": converter_para_python(nb.mean(dados_tendencia_questao_6)),
             "mediana": converter_para_python(nb.median(dados_tendencia_questao_6)),
-            "moda": obter_moda(dados_tendencia_questao_6),
+            "moda": dados_tendencia_questao_6.index(max(dados_tendencia_questao_6)) + 1,
         }
     }
 
